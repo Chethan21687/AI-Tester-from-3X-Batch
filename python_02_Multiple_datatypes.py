75ae3d1cd2ae212ff6a79aca88da044897f4897c
Chethan Dileep <chethan21687@gmail.com>
2026-07-27 13:08:35 +0530

    Add Python programs

---
+++ python_02_Multiple_datatypes.py
@@ -0,0 +1,3 @@
+print('Hello',1234,2.4,'C')
+print('Hello','Chethan',2933,9090,6596,'KA',sep=',')
+print()
---
+++ python_11_Multiple_Prints.py
@@ -0,0 +1,9 @@
+print('Activa','Avenger','Venue')
+
+print('Chethan'+'Moni')
+
+#print('Chethan'+' '+'Moni' + 9090)
+
+#Can't be done as all the values should be of same type. So we need to convert the integer value to string using str() function.
+
+print('Chethan'+' '+'Moni' + str(9090))
