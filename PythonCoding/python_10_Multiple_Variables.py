d0986f3ee9df5a662338d41c74cda0b2ea626848
Chethan Dileep <chethan21687@gmail.com>
2026-07-27 13:14:19 +0530

    Add PythonCoding scripts

---
+++ PythonCoding/python_10_Multiple_Variables.py
@@ -0,0 +1,4 @@
+a,b,c=4.5,'Chethan',9090
+print(a)
+print(b)
+print(c)
---
+++ PythonCoding/python_19_strings.py
@@ -0,0 +1,6 @@
+value=input('Enter your value')
+print('Hello',value)
+print(type(value))
+
+a_int=int(value)
+print(type(a_int))
